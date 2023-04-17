879a97b21f2d3530bfa83a87fec36096c85278ab
Akshay Mahajan <maha0134@algonquinlive.com>
2023-04-17 13:46:55 -0400

    Initial Commit

---
+++ ios-RGB/ios_RGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_RGBApp.swift
+//  ios-RGB
+//
+//  Created by AKSHAY MAHAJAN on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct ios_RGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
